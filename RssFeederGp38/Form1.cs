d8c19fef2c8b277eafe0867dd213479f3a4c2e05
Sonywalk1 <83954101+Sonywalk1@users.noreply.github.com>
2021-11-02 22:10:44 +0100

    Ändartr m,essagebox text

--- RssFeederGp38/Form1.cs
+++ RssFeederGp38/Form1.cs
@@ -164,7 +164,7 @@ namespace RssFeederGp38
             }
             else
             {
-                MessageBox.Show("Must fill");
+                MessageBox.Show("Vänligen fyll i alla textrutor!");
             }
  
         }
@@ -183,16 +183,16 @@ namespace RssFeederGp38
 
             if (valid.Validate(text))
             {
-                //lägger till katergori och uppdaterar med Async. 
+                //lägger till kategori och uppdaterar med Async. 
                 podcastController.CreatePodcast(txtCategoryName.Text, "Category");
             }
             else
             {
-                MessageBox.Show("Must fill");
+                MessageBox.Show("Vänligen fyll i alla textrutor!");
             }
 
             Task task = UpdateCategoryAsync();
-            await task; //avbryter anropsmetoden och ger tillbaka kontrollen till task som kallar tills den väntade uppgiften är klar. 
+            await task; //avbryter anropsmetoden och ger tillbaka kontrollen till task som kallar tills den väntande uppgiften är klar. 
          
         }
 
